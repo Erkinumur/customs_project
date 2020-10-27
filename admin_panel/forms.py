bcb1ea55ad6946ae1bf4437c237004f754dfd3cb
whtslv <vladlee.2001@mail.ru>
2020-10-28 01:27:51 +0600

    deleted form

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -3,8 +3,3 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
-
-class SignUpForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'username', 'last_name', 'password1', 'password2', 'organization', 'inn', 'address', 'okpo']
