73e9b8f9573a1c99f46e19969f4555ff96caef93
whtslv <vladlee.2001@mail.ru>
2020-10-28 01:53:46 +0600

    added verbose names

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
+    user = models.OneToOneField(User,verbose_name='', on_delete=models.CASCADE,
                                 related_name='client')
-    organization = models.CharField(max_length=200)
-    inn = models.PositiveIntegerField()
-    address = models.CharField(max_length=200)
-    okpo = models.PositiveIntegerField()
+    organization = models.CharField(verbose_name='',max_length=200)
+    inn = models.PositiveIntegerField(verbose_name='',)
+    address = models.CharField(verbose_name='',max_length=200)
+    okpo = models.PositiveIntegerField(verbose_name='',)
 
     def __str__(self):
         return f'{self.user}'
@@ -19,17 +19,17 @@ class Client(models.Model):
 
 
 class Driver(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
+    user = models.OneToOneField(User, verbose_name='Водитель', on_delete=models.CASCADE,
                                 related_name='driver')
-    car_number = models.CharField(max_length=50)
-    category = models.CharField(max_length=30)
-    car_model = models.CharField(max_length=100)
-    year_of_issue = models.PositiveIntegerField()
-    colour = models.CharField(max_length=100)
-    chassis = models.CharField(max_length=100)
-    body_type = models.CharField(max_length=100)
-    engine_power = models.CharField(max_length=100)
-    engine_type = models.CharField(max_length=100)
+    car_number = models.CharField(verbose_name='Номер машины', max_length=50)
+    category = models.CharField(verbose_name='Категория',max_length=30)
+    car_model = models.CharField(verbose_name='Модель',max_length=100)
+    year_of_issue = models.PositiveIntegerField(verbose_name='Год выпуска',)
+    color = models.CharField(verbose_name='Цвет',max_length=100)
+    chassis = models.CharField(verbose_name='Шасси',max_length=100)
+    body_type = models.CharField(verbose_name='Кузов',max_length=100)
+    engine_power = models.CharField(verbose_name='Мощность двигателя',max_length=100)
+    engine_type = models.CharField(verbose_name='Тип двигателя',max_length=100)
 
     def __str__(self):
         return f'{self.user}'
@@ -40,13 +40,13 @@ class Driver(models.Model):
 
 
 class Order(models.Model):
-    name = models.CharField(max_length=100)
-    owner = models.ForeignKey(Client, on_delete=models.CASCADE,
+    name = models.CharField(verbose_name='Наименование заказа',max_length=100)
+    owner = models.ForeignKey(Client,verbose_name='Отправитель', on_delete=models.CASCADE,
                               related_name='orders')
-    receiver = models.CharField(max_length=200)
-    invoice = models.FileField(upload_to='files/invoice')
-    packing_list = models.FileField(upload_to='files/packing_list')
-    driver = models.ForeignKey(Driver, on_delete=models.SET_NULL,
+    receiver = models.CharField(verbose_name='Получатель',max_length=200)
+    invoice = models.FileField(verbose_name='Инвойс',upload_to='files/invoice')
+    packing_list = models.FileField(verbose_name='Упаковочный лист',upload_to='files/packing_list')
+    driver = models.ForeignKey(Driver,verbose_name='Водитель', on_delete=models.SET_NULL,
                                related_name='orders', null=True, blank=True)
     
     def __str__(self):
@@ -58,13 +58,12 @@ class Order(models.Model):
 
 
 class Worker(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
+    user = models.OneToOneField(User,verbose_name='Рабочий', on_delete=models.CASCADE,
                                 related_name='worker')
-    organization = models.CharField(max_length=100)
-    phone = models.CharField(max_length=200)
-    passport_id = models.PositiveIntegerField()
-    full_name = models.CharField(max_length=200)
-    place = models.CharField(max_length=200)
+    organization = models.CharField(verbose_name='Название организации',max_length=100)
+    phone = models.CharField(verbose_name='Тел.номер',max_length=200)
+    passport_id = models.PositiveIntegerField(verbose_name='ID паспорта',)
+    place = models.CharField(verbose_name='Адрес',max_length=200)
 
     def __str__(self):
         return f'{self.user}'
