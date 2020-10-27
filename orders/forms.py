3f904c1d3db7362e80af73b8c93d2bd179ef02b1
Erkinumur <erkin.umur94@gmail.com>
2020-10-28 05:55:17 +0600

    modify order lists templates

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,21 @@ from admin_panel.models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({
+            'class': 'login',
+            'placeholder': self.fields['name'].label})
+        self.fields['receiver'].widget.attrs.update({
+            'class': 'login',
+            'placeholder': self.fields['receiver'].label})
+        self.fields['invoice'].widget.attrs.update({
+            'class': 'login',
+            'placeholder': self.fields['invoice'].label})
+        self.fields['packing_list'].widget.attrs.update({
+            'class': 'login',
+            'placeholder': self.fields['packing_list'].label})
+
     class Meta:
         model = Order
         exclude = ['owner', 'driver']
