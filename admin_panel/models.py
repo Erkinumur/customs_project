2b0e08e17edbe82ff6da33a64f1d2e8f084c4f4e
Erkinumur <erkin.umur94@gmail.com>
2020-10-27 21:19:23 +0600

    first commit

---
+++ admin_panel/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='client')
+    full_name = models.CharField(max_length=200)
+
+
+class Driver(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='driver')
+    full_name = models.CharField(max_length=200)
+    car = models.CharField(max_length=100)
+    car_number = models.CharField(max_length=20)
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE,
+                              related_name='orders')
+    weight = models.PositiveIntegerField()
+    volume = models.PositiveIntegerField()
+    driver = models.ForeignKey(Driver, on_delete=models.SET_NULL,
+                               related_name='orders')
+    status = models.CharField(max_length=50)
+
+
+class Worker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='worker')
+    full_name = models.CharField(max_length=200)
+    place = models.CharField(max_length=200)
+
